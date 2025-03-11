692ac5f9e627941591eb967d6cb7c6685bc58d94
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 19:02:18 +0000

    Use Python 3.12 on netlify.

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,5 +1,5 @@
 # Command to build the docs on netfliy.
-cp -r chevah /opt/buildhome/python2.7/lib/python2.7/site-packages/
+cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
 sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy
 echo "Debug result"
 cat deploy/index.html
