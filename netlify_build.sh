5309af611569f267c4f08e5808db5a444ca16d22
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 20:10:29 +0000

    Don't rewrite relative links.

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,7 +2,7 @@
 cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
 sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy
 
-find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="/class="reference internal" href="\/documentation\/sftpplus\/latest\//g' {} +
+find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="[^\.]/class="reference internal" href="\/documentation\/sftpplus\/latest\//g' {} +
 
 echo "Debug result"
 cat deploy/index.html
