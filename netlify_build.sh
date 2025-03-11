0b6b3fed538261bf9a19116a4916333fd4f291f0
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 21:43:54 +0000

    Deploy in full path.

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,8 +1,6 @@
 # Command to build the docs on netfliy.
 cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
-sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy
+mkdir -p deploy/documentation/sftpplus/latests/
+sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/latests/
 
-find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="\([^\.]\)/class="reference internal" href=".\/\1/g' {} +
-
-echo "Debug result"
-cat deploy/index.html
+#find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="\([^\.]\)/class="reference internal" href=".\/\1/g' {} +
