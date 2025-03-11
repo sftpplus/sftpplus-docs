dee20120fbde8849b1e13b8131421aa2f3bcb01c
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 22:41:59 +0000

    Deploy specific version.

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,10 +1,12 @@
 # Command to build the docs on netfliy.
 cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
-mkdir -p deploy/documentation/sftpplus/latest/
+
+#mkdir -p deploy/documentation/sftpplus/latest/
+mkdir -p deploy/documentation/sftpplus/v/5.10.0/
 
 # Build the standalone version in root.
 sphinx-build -b html --keep-going -W -D html_theme=standalone -A robots="$DOCS_ROBOT" src/doc_source/ deploy/
 # Build the integrated version to a path that will match the website.
-sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/latest/
+sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/v/5.10.0/
 
 ls -al deploy/
