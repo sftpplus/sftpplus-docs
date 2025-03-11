dacd252f3f1f7dd9ec764482f44bfd48e1f602b1
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 21:57:13 +0000

    Build both standalone and integrated versions.

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,7 +1,9 @@
 # Command to build the docs on netfliy.
 cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
 mkdir -p deploy/documentation/sftpplus/latest/
+sphinx-build -b html --keep-going -W -D html_theme=standalone -A robots="$DOCS_ROBOT" src/doc_source/ deploy/
 sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/latest/
 
 #find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="\([^\.]\)/class="reference internal" href=".\/\1/g' {} +
-ls -al deploy/documentation/sftpplus/latest/
+
+ls -al deploy/
