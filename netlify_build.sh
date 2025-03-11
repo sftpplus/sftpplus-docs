ff09793c0c82e270dc2a8843d2ac26c6eeaa5c33
Adi Roiban <adi.roiban@chevah.com>
2025-03-11 21:52:04 +0000

    Fix link.

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,7 +1,7 @@
 # Command to build the docs on netfliy.
 cp -r chevah /opt/buildhome/python3.12/lib/python3.12/site-packages/
-mkdir -p deploy/documentation/sftpplus/latests/
-sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/latests/
+mkdir -p deploy/documentation/sftpplus/latest/
+sphinx-build -b html --keep-going -W -D html_theme=integrated -A robots="$DOCS_ROBOT" src/doc_source/ deploy/documentation/sftpplus/latest/
 
 #find deploy -type f -name '*.html' -exec sed -i 's/class="reference internal" href="\([^\.]\)/class="reference internal" href=".\/\1/g' {} +
-ls deploy
+ls -al deploy/documentation/sftpplus/latest/
