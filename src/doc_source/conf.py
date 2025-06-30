83508da18f17e7c7b56ca83e22452a421970a4a5
Adi Roiban <adi.roiban@chevah.com>
2025-06-30 09:28:59 +0100

    Update version.

--- src/doc_source/conf.py
+++ src/doc_source/conf.py
@@ -1,9 +1,5 @@
 
 
-extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    ]
 suppress_warnings = ['toc.secnum']
 source_suffix = '.rst'
 # Ignore included files in root and in child folders from being reported
@@ -14,23 +10,25 @@ pygments_style = 'sphinx'
 smartquotes = False
 html4_writer = False
 html_experimental_html5_writer = True
-templates_path = ["../../sphinx"]
+templates_path = ['/home/adi/chevah/server/build-py3/lib/python3.12/site-packages/sftpplus_website/sphinx']
+
 html_static_path = ['_static']
 html_extra_path = ['versions.js']
-html_theme_path = ["../../sphinx"]
+html_theme_path = ['/home/adi/chevah/server/build-py3/lib/python3.12/site-packages/sftpplus_website/sphinx']
 html_theme = 'integrated'
+
 project = "SFTPPlus"
 copyright = "ProAtria Team"
 
-version = "5.13.0"
-release = "5.13.0"
+version = "5.14.0"
+release = "5.14.0"
 
 autodoc_default_flags = ['members']
 primary_domain = 'py'
 
 pdf_documents = [(
     'index',
-    'SFTPPlus-5.13.0',
+    'SFTPPlus-5.14.0',
     'SFTPPlus Documentation',
     'ProAtria Team',
     )]
@@ -41,10 +39,17 @@ pdf_toc_depth = 2
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
+    'sphinx_copybutton',
+    'sphinx_design',
     ]
 
 html_context = {
 "wip_redirect": "",
 "robots": "noindex, nofollow",
 "canonical_site": "https://www.sftpplus.com/documentation/sftpplus/latest/",
+"source_type": "github",
+"source_user": "sftpplus",
+"source_repo": "sftpplus-docs",
+"source_version": "main",
+"source_docs_path": "/src/doc_source/",
 }
