eb1759be63df4936ab5e13f4f80c101a9e7475de
Adi Roiban <adiroiban@gmail.com>
2025-10-15 22:03:26 +0100

    Fix latest version.

--- docs/conf.py
+++ docs/conf.py
@@ -20,15 +20,15 @@ html_theme = 'integrated'
 project = "SFTPPlus"
 copyright = "ProAtria Team"
 
-version = "5.17.0"
-release = "5.17.0"
+version = "5.18.0"
+release = "5.18.0"
 
 autodoc_default_flags = ['members']
 primary_domain = 'py'
 
 pdf_documents = [(
     'index',
-    'SFTPPlus-5.17.0',
+    'SFTPPlus-5.18.0',
     'SFTPPlus Documentation',
     'ProAtria Team',
     )]
